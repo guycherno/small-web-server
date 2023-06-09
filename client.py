54e05fbf016d3431dc80c20d4381900ecf98294d
guycherno <72074365+guycherno@users.noreply.github.com>
2023-06-09 17:44:29 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8000"  # Replace with your WebSocket server URI
+
+    async with websockets.connect(uri) as websocket:
+        # Send a message to the server
+        message = "Hello, server!"
+        await websocket.send(message)
+        print(f"Sent: {message}")
+
+        # Receive a response from the server
+        response = await websocket.recv()
+        print(f"Received: {response}")
+
+# Run the WebSocket client
+asyncio.get_event_loop().run_until_complete(hello())
