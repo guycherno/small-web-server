b70d1447eac015cdfa9ed1265fcde89a83a5f2f3
guycherno <72074365+guycherno@users.noreply.github.com>
2023-06-09 17:40:06 +0300

    add websocket

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
 const app = express();
 const port = 3000;
 
+// Handle WebSocket connection
+wss.on('connection', (ws) => {
+  // Handle incoming messages
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+    // Send a response back to the client
+    ws.send('Server received: ' + message);
+  });
+
+  // Handle client disconnection
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
@@ -9,3 +27,11 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
